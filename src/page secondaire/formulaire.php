c38d9e42135efb55f4b129c0c53c16af8fb4cb58
Huitieme <Huitiemekabuzi@gmail.com>
2024-06-21 23:20:54 +0200

    connexion

--- src/page secondaire/formulaire.php
+++ src/page secondaire/formulaire.php
@@ -6,31 +6,37 @@
     <link rel="stylesheet" href="form.css">
     <title>Document</title>
 </head>
-<body>
-    <div class="weka">
-        <form action="#" method="post">
-            <h1>contactez nous</h1>
+<body class="flex justify-center items-center min-h-dvh ">
+    <div class="w-420 bg-slate-200" >
+        <form action="">
+            <h1 class="text-2xl text-center box-border h-32 w-32 p-4 border-4" >contactez nous</h1>
             <div class="solutech">
                 <input type="text" placeholder="se connecter avec facebook">
             </div>
-            <div class="solutech">
+            <div class="ad">
+                <img src="facebook.png" alt="fb">
+            </div>
+            
+            <div class="m-30 0 relative">
                 <input type="text" placeholder="se connecter avec Google">
             </div>
-
-            <div class="h">
+             <div class="add">
+                <img src="google.png" alt="">
+             </div>
+           <div class="h">
             <hr>
             <p>ou</p>
             <hr>
            </div>
 
-            <div class="goma">
-                <input type="email" name="email" placeholder="Adresse mail">
+            <div class="m-30 0">
+                <input type="mail" placeholder="Adresse mail">
             </div>
-            <div class="goma">
-                <input type="password" name="password" placeholder="Mots passe">
+            <div class="m-30 0">
+                <input type="password" placeholder="Mots passe">
             </div>
             <div class="btn" align="center">
-                <input type="submit" name="valider" value="Se connecter">
+                <input type="submit" value="Se connecter">
             </div>
         </form>
     </div>
